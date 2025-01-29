7107d934584f110df17df7ca7dd021d2c4a1a945
Nicholas Swan <nicholasgswan@gmail.com>
2025-01-28 23:29:31 -0600

    continuing configuration

--- src/main/java/com/nicholas/receipts/controllers/ReceiptsController.java
+++ src/main/java/com/nicholas/receipts/controllers/ReceiptsController.java
@@ -1,18 +1,47 @@
 package com.nicholas.receipts.controllers;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import com.nicholas.receipts.models.Receipt;
+import com.nicholas.receipts.repository.ReceiptRepository;
+
+import jakarta.persistence.EntityManagerFactory;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 
 @RestController
+
 public class ReceiptsController {
+
+    
+    ReceiptRepository receiptRepository;
+    
+    public ReceiptsController (ReceiptRepository receiptRepository){
+        this.receiptRepository = receiptRepository;
+    }
 
     @GetMapping("/")
     public String index() {
         return "Hello this project works";
     }
 
+    @PostMapping("/receipts")
+    public Receipt postMethodName(@RequestBody Receipt entity) {
+        
+        receiptRepository.save(entity);
+
+        return entity;
+    }
+    
     
     
 }
--- src/main/java/com/nicholas/receipts/models/Receipt.java
+++ src/main/java/com/nicholas/receipts/models/Receipt.java
@@ -5,30 +5,57 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.List;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.Data;
 
 @Entity
+@Table
+@Data
 public class Receipt {
     
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    String receiptId;
-    String retailer;
-    LocalDate date;
-    LocalTime time;
-    List<Item> items;
-
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long receiptId;
+    @Column
+    private String retailer;
+    @Column
+    @JsonProperty("purchaseDate")
+    private LocalDate date;
+    @Column
+    @JsonProperty("purchaseTime")
+    private LocalTime time;
+    @OneToMany(mappedBy = "receipt")
+    @JsonProperty("items")
+    private List<Item> items;
+    @Column
+    private BigDecimal total;
     
     @Entity
+    @Table
+    @Data
     private static class Item {
         @Id
-        @GeneratedValue
-        String itemId;
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        Long itemId;
+        @Column
         String shortDescription;
+        @Column
         BigDecimal price;
+        @ManyToOne
+        @JoinColumn(name="receipt_id", nullable = false)
+        Receipt receipt;
     }
 
 
